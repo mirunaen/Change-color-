00c2a3fefdb9b4a150add88a79f41ef2e99f3180
mirunaen <mirunaen@gmail.com>
2020-06-07 20:25:41 +0200

    script.js

--- HexColor/script.js
+++ HexColor/script.js
@@ -2,3 +2,15 @@ const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const hexBtn = document.querySelector(".btn");
 const body = document.querySelector("body");
 const hex = document.querySelector(".hex");
+
+hexBtn.addEventListener("click", getHex);
+
+function getHex() {
+  let hexCol = "#";
+
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hexNumbers.length);
+    hexCol += hexNumbers[random];
+    console.log(hexCol);
+  }
+}
