a43e9dbed35f7d9fe2eb04c51b6092032af09030
mirunaen <mirunaen@gmail.com>
2020-06-07 19:30:21 +0200

    js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const colorBtn = document.querySelector(".btn");
 const bodyBcg = document.querySelector("body");
 
-const colors = ["yellow", "red", "green", "#3b5998"];
+const colors = [
+  "yellow",
+  "red",
+  "green",
+  "#3b5998",
+  "#FAEBD7",
+  "#00FFFF",
+  "#F0F8FF",
+  "#7FFF00",
+  "#D2691E",
+  "#6495ED",
+  "#B8860B",
+  "#DC143C",
+];
 
 colorBtn.addEventListener("click", changeColor);
 
 function changeColor() {
-  bodyBcg.style.backgroundColor = colors[2];
+  // bodyBcg.style.backgroundColor = colors[2];
+  let random = Math.floor(Math.random() * colors.length);
+  bodyBcg.style.backgroundColor = colors[random];
 }
