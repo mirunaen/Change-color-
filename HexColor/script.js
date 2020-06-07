077b506260617fba3d33d07f0580bd8e0bd7ae75
mirunaen <mirunaen@gmail.com>
2020-06-07 19:51:01 +0200

    added const

---
+++ HexColor/script.js
@@ -0,0 +1,4 @@
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hexBtn = document.querySelector(".btn");
+const body = document.querySelector("body");
+const hex = document.querySelector(".hex");
